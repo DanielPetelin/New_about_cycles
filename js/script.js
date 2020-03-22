3249340f639bff365af160372e7ef98e87dd6ff3
Daniil Petelin <DaniilPetelin77@gmail.com>
2020-03-22 12:39:56 +0300

    add little jscode

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
 document.getElementById('push').onclick = function() {
     var r = document.getElementsByName('r');
+    // var r = document.querySelectorAll('input[type="radio"]');
     for (var i = 0; i < r.length; i++) {
         // console.log(i);
-        if(r[i].checked) {
+        if (r[i].checked) {
             console.log(r[i].value);
         };
     };
 };
+
+var out = '';
+
+for (var i = 0; i < 10; i++) {
+    if (i == 6) {
+        continue;
+    };
+    out += i + ' ';
+    if (i == 6) {
+        break;
+    };
+};
+
+document.getElementById('out').innerHTML = out;
